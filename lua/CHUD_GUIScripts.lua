88f76beb3fe6befcc28d7e90119e7344ca868e8f
Mendasp <mendasp@gmail.com>
2014-04-27 15:07:47 +0200

    Last piece of the puzzle!

--- lua/CHUD_GUIScripts.lua
+++ lua/CHUD_GUIScripts.lua
@@ -15,3 +15,6 @@ Script.Load("lua/CHUD_GUIScripts/GUIUnitStatus.lua")
 Script.Load("lua/CHUD_GUIScripts/GUIGameEnd.lua")
 Script.Load("lua/CHUD_GUIScripts/GUIScoreboard.lua")
 Script.Load("lua/CHUD_GUIScripts/GUIVoiceChat.lua")
+
+// I suppose this isn't really a GUIScript, but... it still draws stuff on screen? :D
+Script.Load("lua/CHUD_GUIScripts/CystGhostModel.lua")
