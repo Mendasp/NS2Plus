62d8e7a514b8d15dac85a67a00276e4eb32f7bf0
sclark39 <sclark@freolic.com>
2014-05-08 10:14:52 -0700

    Fixing lights staying red if powerpoint is under attack (by infestation) when rebuilt

--- lua/Server/CHUD_Server.lua
+++ lua/Server/CHUD_Server.lua
@@ -11,6 +11,7 @@ Script.Load("lua/Server/CHUD_ServerStats.lua")
 Script.Load("lua/Server/CHUD_ClientOptions.lua")
 Script.Load("lua/Server/CHUD_MarineTeam.lua")
 Script.Load("lua/Server/CHUD_PlayerInfo.lua")
+Script.Load("lua/Server/CHUD_PowerPoint.lua")
 
 local oldBadgesActive = false
 // Warning about outdated mod
