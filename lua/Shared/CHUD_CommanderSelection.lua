f1217913f9a9362cb79c09b5aa976c2ebc3eb710
Mendasp <mendasp@gmail.com>
2014-05-08 17:38:02 +0200

    Added comments to the comm selection bugfix

--- lua/Shared/CHUD_CommanderSelection.lua
+++ lua/Shared/CHUD_CommanderSelection.lua
@@ -2,24 +2,25 @@ Script.Load("lua/Commander_Selection.lua")
 Class_ReplaceMethod( "Commander", "SelectHotkeyGroup", 
 	function(self, number)
 
-	if Client then    
-		self:SendSelectHotkeyGroupMessage(number)        
-	end
-	
-	local selection = false
-
-	// select entities which match hotgroup, unselect all others
-	for _, entity in ipairs(GetEntitiesWithMixin("Selectable")) do
-		
-		if entity:GetHotGroupNumber() == number and not GetAreEnemies(self, entity) then
-			entity:SetSelected(self:GetTeamNumber(), true, true, false)
-			selection = true
-		else
-			entity:SetSelected(self:GetTeamNumber(), false, true, false)
+		if Client then    
+			self:SendSelectHotkeyGroupMessage(number)        
 		end
 		
-	end
-	
-	UpdateMenuTechId(self:GetTeamNumber(), selection)
+		local selection = false
+
+		for _, entity in ipairs(GetEntitiesWithMixin("Selectable")) do
+			
+			// For some reason the latest selected alien entity will be associated with whatever hotgroup you select
+			// Deselect if they're enemies (vanilla only checks for the commander's own team)
+			if entity:GetHotGroupNumber() == number and not GetAreEnemies(self, entity) then
+				entity:SetSelected(self:GetTeamNumber(), true, true, false)
+				selection = true
+			else
+				entity:SetSelected(self:GetTeamNumber(), false, true, false)
+			end
+			
+		end
+		
+		UpdateMenuTechId(self:GetTeamNumber(), selection)
 
 	end)
