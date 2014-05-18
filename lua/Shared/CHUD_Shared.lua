f35653d6fefc2ef3116fd714f1a5fc18f9258cb6
sclark39 <sclark@freolic.com>
2014-05-18 12:59:52 -0700

    Fixed Grenade collision radius to be more representative of the visual model -- proximity detonation radius is unchanged

--- lua/Shared/CHUD_Shared.lua
+++ lua/Shared/CHUD_Shared.lua
@@ -29,6 +29,7 @@ Script.Load("lua/Shared/CHUD_Autopickup.lua")
 Script.Load("lua/Shared/CHUD_CommanderSelection.lua")
 Script.Load("lua/Shared/CHUD_LayMines.lua")
 Script.Load("lua/Shared/CHUD_AmmoPack.lua")
+Script.Load("lua/Shared/CHUD_Grenade.lua")
 
 CHUDTagBitmask = {
 	mcr = 0x1,
