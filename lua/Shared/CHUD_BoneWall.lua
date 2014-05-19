d2519bbc48e38b3bc345b66218a74ff3bb9f5630
sclark39 <sclark@freolic.com>
2014-05-19 10:29:59 -0700

    Fixing enum appending for bone wall, making bone wall blip a closer representation of actual size, removing CompMod compatibility with kBlipInfo

--- lua/Shared/CHUD_BoneWall.lua
+++ lua/Shared/CHUD_BoneWall.lua
@@ -1,20 +1,4 @@
--- we need to completly replace the kMinimapBlipType enum because an enum cannot
--- be altered after creation. so, we're basically going to clone as just a
--- regular table instead of an enum. should be fine I guess, apparently enums
--- are just tables anyway.
-local blipTypes = {}
-local maxVal, key = 0
-for k, v in pairs(kMinimapBlipType) do
-	if type(v) == number and v > maxVal then
-		maxVal, key = v, k
-	end
-	blipTypes[k] = v
-end
-
--- append the entry for BoneWall, effectively registering it as a new blip type
-blipTypes["BoneWall"] = maxVal + 1
-blipTypes[maxVal + 1] = "BoneWall"
-kMinimapBlipType = blipTypes
+AppendToEnum( kMinimapBlipType, "BoneWall" )
 
 -- ClassToGrid is a map of class names to a grid position on the spritesheet
 -- ui/minimap_blip.dds and it's defined in NS2Utility.lua
