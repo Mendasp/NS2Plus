d3b48d08174b2c9799ba91d7389cb2b3eef432a4
sclark39 <sclark@freolic.com>
2014-05-25 16:53:16 -0700

    Add a min-life test for a grenade's larger detonation radius check

--- lua/Shared/CHUD_Grenade.lua
+++ lua/Shared/CHUD_Grenade.lua
@@ -11,13 +11,17 @@ if Server then
 			
 			local endTrace = self:GetOrigin()
 			
-			local trace = Shared.TraceCapsule( startTrace, endTrace, Grenade.kDetonateRadius,  0, CollisionRep.Damage, PhysicsMask.PredictedProjectileGroup, EntityFilterOne(self) )
-			
-			if trace.fraction ~= 1 then
-				if GetAreEnemies(self, trace.entity) then
-					self:SetOrigin( trace.endPoint )
-					self:Detonate( trace.entity )
-				end
-			end		
+			local controller = self.projectileController
+            local oldEnough = controller and ( controller.minLifeTime + controller.creationTime <= Shared.GetTime() )
+			if oldEnough then
+				local trace = Shared.TraceCapsule( startTrace, endTrace, Grenade.kDetonateRadius,  0, CollisionRep.Damage, PhysicsMask.PredictedProjectileGroup, EntityFilterOne(self) )
+				
+				if trace.fraction ~= 1 then
+					if GetAreEnemies(self, trace.entity) then
+						self:SetOrigin( trace.endPoint )
+						self:Detonate( trace.entity )
+					end
+				end		
+			end
 		end)
 end
