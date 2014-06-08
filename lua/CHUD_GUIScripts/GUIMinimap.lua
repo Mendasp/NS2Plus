4582e2b7727934cb90c1d69ab58d6e88bfaccfae
sclark39 <sclark@freolic.com>
2014-06-08 09:05:34 -0700

    Fix potential issue in vanilla with GUIMinimap trying to call GUIItem.SetText with a nil value

--- lua/CHUD_GUIScripts/GUIMinimap.lua
+++ lua/CHUD_GUIScripts/GUIMinimap.lua
@@ -151,22 +151,44 @@ originalLocationNameInit = Class_ReplaceMethod( "GUIMinimap", "InitializeLocatio
 		end
 	end)
 
-local GetNamePool
-local updateStaticBlips, oldDrawMinimapNames = LocateUpValue( GUIMinimap.Update, "DrawMinimapNames", { LocateRecurse = true } )
+
+local GetNamePool, OnSameMinimapBlipTeam, kPlayerNameColorMarine, kPlayerNameColorAlien, namePos, kPlayerNameOffset
 local function NewDrawMinimapNames(self, shouldShowPlayerNames, clientIndex, spectating, blipTeam, xPos, yPos, isParasited)
 
-	oldDrawMinimapNames( self, shouldShowPlayerNames, clientIndex, spectating, blipTeam, xPos, yPos, isParasited )
-	
-	if clientIndex > 0 and shouldShowPlayerNames and not isParasited and not spectating then				
+	if clientIndex > 0 and shouldShowPlayerNames then
+		
 		local record = Scoreboard_GetPlayerRecord( clientIndex )			
-		if record.IsRookie then
-			local nameItem = GetNamePool(self, clientIndex)		
-			nameItem:SetColor( Color( 0, 1, 0 ) )
+		
+		if record and record.Name then			
+			local nameItem = GetNamePool(self, clientIndex)
+			nameItem:SetIsVisible(true)	
+			nameItem:SetText(record.Name)
+			
+			local nameColor = Color(1, 1, 1)
+			if isParasited then
+				nameColor.b = 0
+			elseif spectating then
+				if OnSameMinimapBlipTeam(kMinimapBlipTeam.Marine, blipTeam) then
+					nameColor = kPlayerNameColorMarine
+				else
+					nameColor = kPlayerNameColorAlien
+				end
+			elseif record.IsRookie then
+				nameColor = Color( 0, 1, 0 )
+			end
+			
+			nameItem:SetColor(nameColor)
+			
+			namePos.x = xPos
+			namePos.y = yPos - kPlayerNameOffset
+			nameItem:SetPosition(namePos)
 		end
+		
 	end
 	
-end		
-ReplaceUpValue( updateStaticBlips, "DrawMinimapNames", NewDrawMinimapNames, { CopyUpValues = true } )
+end
+ReplaceUpValue( GUIMinimap.Update, "DrawMinimapNames", NewDrawMinimapNames, { CopyUpValues = true; LocateRecurse = true; } )
+
 
 local originalMinimapSendKeyEvent
 originalMinimapSendKeyEvent = Class_ReplaceMethod( "GUIMinimap", "SendKeyEvent",
