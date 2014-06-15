85508bfffcd97eb0b0d7d05e132b83ccb455e809
Mendasp <mendasp@gmail.com>
2014-06-15 17:05:59 +0200

    Red outline for HMG (only used if CompMod v3 detected)

---
+++ lua/CHUD/Client/CHUD_EquipmentOutline.lua
@@ -0,0 +1,140 @@
+// ======= Copyright (c) 2012, Unknown Worlds Entertainment, Inc. All rights reserved. ==========    
+//    
+// lua\EquipmentOutline.lua    
+//    
+//    Created by:   Andreas Urwalek (andi@unknownworlds.com) and
+//                  Max McGuire (max@unknownworlds.com)
+//    
+// ========= For more information, visit us at http://www.unknownworlds.com =====================    
+
+local _invRenderMask = bit.bnot(0x3c)
+local _maxDistance = 38
+local _maxDistance_Commander = 60
+local _enabled = true
+
+local lookup = { "GrenadeLauncher", "Shotgun", "Flamethrower" }
+local renderMasks = { [0] = 0x04, 0x08, 0x10, 0x20 }
+local effectName = "shaders/EquipmentOutline.screenfx"
+local cameras = {}
+
+// Do this only for CompMod v3
+if rawget(kTechId, "HeavyMachineGun") then
+	_invRenderMask = bit.bnot(0x7c)
+	lookup = { "GrenadeLauncher", "Shotgun", "Flamethrower", "HeavyMachineGun" }
+	renderMasks = { [0] = 0x04, 0x08, 0x10, 0x20, 0x40 }
+	effectName = "shaders/EquipmentOutlineCM.screenfx"
+end
+
+function EquipmentOutline_Initialize()
+	
+	for i=0,#lookup do
+		
+		local camera = Client.CreateRenderCamera()
+		camera:SetTargetTexture("*equipment_outline"..i, true)
+		camera:SetRenderMask(renderMasks[i])
+		camera:SetIsVisible(false)
+		camera:SetCullingMode(RenderCamera.CullingMode_Frustum)
+		camera:SetRenderSetup("shaders/Mask.render_setup")
+		cameras[i] = camera
+	
+	end
+
+	local screenEffect = Client.CreateScreenEffect(effectName)
+	screenEffect:SetActive(false)
+	EquipmentOutline_screenEffect = screenEffect
+end
+
+function EquipmentOutline_Shudown()
+	for i=0,#lookup do	
+		Client.DestroyRenderCamera( cameras[i] )
+		cameras[i] = nil	
+	end
+	Client.DestroyScreenEffect( EquipmentOutline_screenEffect )
+	EquipmentOutline_screenEffect = nil
+end
+
+/** Enables or disabls the hive vision effect. When the effect is not needed it should 
+ * be disabled to boost performance. */
+function EquipmentOutline_SetEnabled(enabled)
+
+	for i=0,#lookup do
+		cameras[i]:SetIsVisible(enabled and _enabled)
+    end
+	
+	EquipmentOutline_screenEffect:SetActive(enabled and _enabled)
+end
+
+/** Must be called prior to rendering */
+function EquipmentOutline_SyncCamera(rendercamera, forCommander)
+
+    local distance = ConditionalValue(forCommander, _maxDistance_Commander, _maxDistance)
+	
+	for i=0,#lookup do
+		local camera = cameras[i]
+		camera:SetCoords(rendercamera:GetCoords())
+		camera:SetFov(rendercamera:GetFov())
+		camera:SetFarPlane(distance + 1)
+	end
+	
+	local screenEffect = EquipmentOutline_screenEffect		
+	
+	screenEffect:SetParameter("time", Shared.GetTime())
+	screenEffect:SetParameter("maxDistance", distance)
+end
+
+/** Adds a model to the hive vision */
+function EquipmentOutline_AddModel(model,weaponclass)
+
+    local renderMask = model:GetRenderMask()
+    model:SetRenderMask(bit.bor(renderMask, renderMasks[weaponclass or 0]))
+    
+end
+
+/** Removes a model from the hive vision */
+function EquipmentOutline_RemoveModel(model)
+
+    local renderMask = model:GetRenderMask()
+    model:SetRenderMask(bit.band(renderMask, _invRenderMask))
+    
+end
+
+function EquipmentOutline_UpdateModel(forEntity)
+
+    local player = Client.GetLocalPlayer()
+    
+    // Check if player can pickup this item or if player is a spectator
+	local highlightDroppedWeapon = player ~= nil and ((player:GetTeamNumber() == kSpectatorIndex and Client.GetOutlinePlayers()) or player:isa("MarineCommander")) and forEntity:isa("Weapon") and forEntity.weaponWorldState == true
+    local visible = (player ~= nil and forEntity:GetIsValidRecipient(player)) or highlightDroppedWeapon
+    local model = HasMixin(forEntity, "Model") and forEntity:GetRenderModel() or nil
+    
+	if forEntity:isa("WeaponAmmoPack") then
+		model = forEntity:GetRenderModel() or nil
+		visible = player ~= nil and player:GetActiveWeapon() and player:GetActiveWeapon():isa(forEntity:GetWeaponClassName())
+	end
+	
+	local weaponclass = 0
+	for i=1,#lookup do
+		if forEntity:isa( lookup[i] ) then
+			weaponclass = i
+			break
+		end
+	end
+	
+	// Update the visibility status.
+	if model and visible ~= model.equipmentVisible then    
+		if visible then
+			EquipmentOutline_AddModel(model,weaponclass)
+		else
+			EquipmentOutline_RemoveModel(model)
+		end
+		model.equipmentVisible = visible
+		
+	end
+
+end
+
+// For debugging.
+local function OnCommandOutline(enabled)
+    _enabled = enabled ~= "false"
+end
+Event.Hook("Console_outline", OnCommandOutline)
---
+++ shaders/EquipmentOutlineCM.hlsl
@@ -0,0 +1,110 @@
+#include <renderer/RenderSetup.hlsl>
+
+struct VS_INPUT
+{
+    float3 ssPosition   : POSITION;
+    float2 texCoord     : TEXCOORD0;
+};
+
+struct VS_OUTPUT
+{
+    float2 texCoord     : TEXCOORD0;
+    float4 ssPosition   : SV_POSITION;
+};
+
+struct PS_INPUT
+{
+    float2 texCoord     : TEXCOORD0;
+};
+
+sampler2D     inputTexture;
+sampler2D     inputTexture1;
+sampler2D     inputTexture2;
+
+sampler2D     outlineTex0;
+sampler2D     outlineTex1;
+sampler2D     outlineTex2;
+sampler2D     outlineTex3;
+sampler2D     outlineTex4;
+
+    
+/**
+ * Vertex shader.
+ */  
+VS_OUTPUT SFXBasicVS(VS_INPUT input)
+{
+
+    VS_OUTPUT output;
+
+    output.ssPosition = float4(input.ssPosition, 1);
+    output.texCoord   = input.texCoord + texelCenter;
+
+    return output;
+
+}
+
+float4 MaskPS(PS_INPUT input) : COLOR0
+{
+	float equip0 = tex2D( outlineTex0, input.texCoord ).r;
+	float equip1 = tex2D( outlineTex1, input.texCoord ).r;
+	float equip2 = tex2D( outlineTex2, input.texCoord ).r;
+	float equip3 = tex2D( outlineTex3, input.texCoord ).r;
+	float equip4 = tex2D( outlineTex4, input.texCoord ).r;
+
+	if ( equip4 > 0 ) {
+        return float4(1, 0, 1, 1);
+	} else if ( equip3 > 0 ) {
+        return float4(1, 0, 0, 1);
+	} else if ( equip2 > 0 ) {
+		return float4(0, 1, 0, 1 );
+	} else if ( equip1 > 0 ) {
+		return float4(0, 0, 1, 1 );
+	} else if ( equip0 > 0 ) {
+		return float4(0,0,0,1 );	
+	}
+    return float4(0, 0, 0, 0);
+}
+ 
+float4 DownSamplePS(PS_INPUT input) : COLOR0
+{
+    return tex2D( inputTexture, input.texCoord );
+ }
+
+float4 CompositePS(PS_INPUT input) : COLOR0
+{
+    return tex2D( inputTexture1, input.texCoord) + tex2D( inputTexture2, input.texCoord);
+}
+
+float4 FinalCompositePS(PS_INPUT input) : COLOR0
+{
+
+    float4 result = tex2D(inputTexture, input.texCoord);
+
+    // Blend in the outlines of objects visible through the walls.
+    float4 glow = tex2D(inputTexture1,  input.texCoord);
+    if (glow.a > 0)
+    {
+		float4 mask = tex2D( inputTexture2, input.texCoord);
+		float4 glowColor = mask;
+		if ( glow.r > 0 && glow.b > 0 ) {
+			glowColor = float4(4, 0, 0, glow.a);
+		} else if ( glow.r > 0 ) {
+			glowColor = float4(3, 3, 0, glow.a);
+		} else if( glow.g > 0 ) {
+			glowColor = float4(0, 4, 0, glow.a);
+		} else if( glow.b > 0 ) {
+			glowColor = float4(3, 0, 3, glow.a);
+		} else {
+			glowColor = float4(0, 3, 5, glow.a);
+		}
+		
+		glow.rgb = glow.aaa;
+		
+        float opacity = 1 - mask.a;
+        result += glow * glowColor * opacity * 0.2;  
+    }
+
+    return result;
+
+}
+
