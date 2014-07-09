d2d7ce6970d9fcb914935326c0734083a5ed175c
Mendasp <mendasp@gmail.com>
2014-07-09 15:32:54 +0200

    Made unsocketed/blueprint power nodes show up properly on minimap

---
+++ lua/NS2Plus/Shared/CHUD_BlueprintPowerPoint.lua
@@ -0,0 +1,27 @@
+AppendToEnum( kMinimapBlipType, "BlueprintPowerPoint" )
+
+local originalGetMapBlipInfo = MapBlipMixin.GetMapBlipInfo
+	function MapBlipMixin:GetMapBlipInfo()
+		if self:isa("PowerPoint") then
+			if self.OnGetMapBlipInfo then
+				return self:OnGetMapBlipInfo()
+			end
+
+			local success = false
+			local blipType = kMinimapBlipType.Undefined
+			local blipTeam = -1
+			local isAttacked = HasMixin(self, "Combat") and self:GetIsInCombat()
+			local isParasited = HasMixin(self, "ParasiteAble") and self:GetIsParasited()
+			
+			blipType = ConditionalValue( self:GetIsDisabled(), kMinimapBlipType.DestroyedPowerPoint, ConditionalValue(self:GetCanTakeDamageOverride(), kMinimapBlipType.PowerPoint, kMinimapBlipType.BlueprintPowerPoint))
+			blipTeam = self:GetTeamNumber()
+			
+			if blipType ~= 0 then
+				success = true
+			end
+			
+			return success, blipType, blipTeam, isAttacked, isParasited
+		else
+			return originalGetMapBlipInfo(self)
+		end
+	end
