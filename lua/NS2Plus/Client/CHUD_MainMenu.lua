cf72a631927db7c4a6e13453929b152052385948
Mendasp <mendasp@gmail.com>
2014-07-17 03:32:45 +0200

    Updated main menu

--- lua/NS2Plus/Client/CHUD_MainMenu.lua
+++ lua/NS2Plus/Client/CHUD_MainMenu.lua
@@ -69,55 +69,23 @@ originalMenuCreateOptions = Class_ReplaceMethod( "GUIMainMenu", "CreateOptionsFo
 		return form
 	end)
 	
-local menuLinks = { }
-originalInitMainMenu = Class_ReplaceMethod( "GUIMainMenu", "Initialize",
-	function(self)
-	
-		LoadCSSFile("lua/NS2Plus/Client/chud.css")
-
+originalCreateMainLinks = Class_ReplaceMethod( "GUIMainMenu", "CreateMainLinks", function(self)
 		mainMenu = self
-		local optionsNr
-		// Override CreateMainLink so we can get a table with menu entries
-		// When we have a table with main menu entries we can sort it ourselves
-		// Seems like the positions are 70px apart looking at the CSS, so we can do this in code!
-		originalCreateMainLink = Class_ReplaceMethod( "GUIMainMenu", "CreateMainLink",
-			function(self, text, className, linkNum)
-				if className == "options_ingame" then
-					optionsNr = linkNum
-				end
-				if optionsNr and optionsNr <= linkNum and text ~= "NS2+ OPTIONS" then
-					linkNum = tostring(tonumber(linkNum)+1)
-					if tonumber(linkNum) < 10 then
-						linkNum = "0" .. linkNum
-					end
-				end
-				local menuLink = originalCreateMainLink(self, text, className, linkNum)
-				menuLink.linkNr = tonumber(linkNum)-1
-				table.insert(menuLinks, menuLink)
-				return menuLink
-			end)
-
-		originalInitMainMenu(self)
-        self.CHUDOptionLink = self:CreateMainLink("NS2+ OPTIONS", "options_ingame", "06")
-        self.CHUDOptionLink:AddEventCallbacks(
-        {
-            OnClick = function(self)
-            
-                if not mainMenu.CHUDOptionWindow then
-					mainMenu:CreateCHUDOptionWindow()
-                end
-                mainMenu:TriggerOpenAnimation(mainMenu.CHUDOptionWindow)
-                mainMenu:HideMenu()
-                
-            end
-        })
-
-		for _, menuLink in pairs(menuLinks) do
-			menuLink:SetTopOffset(50+70*menuLink.linkNr)
+		local OnClick = function(self)            
+			if not self.scriptHandle.CHUDOptionWindow then
+				self.scriptHandle:CreateCHUDOptionWindow()
+			end
+			self.scriptHandle:TriggerOpenAnimation(self.scriptHandle.CHUDOptionWindow)
+			self.scriptHandle:HideMenu()
+		end
+		self:AddMainLink( "NS2+ OPTIONS", 6, OnClick, 2)
+		originalCreateMainLinks(self)
+		
+		for i, menuLink in pairs(self.Links) do
+			menuLink:SetTopOffset(50+70*(i-1))
 		end
 		
 		self.profileBackground:SetTopOffset(-70)
-		
 	end)
 	
 originalHideMenu = Class_ReplaceMethod( "GUIMainMenu", "HideMenu",
@@ -127,30 +95,13 @@ originalHideMenu = Class_ReplaceMethod( "GUIMainMenu", "HideMenu",
 		end
 	
 		originalHideMenu(self)
-		
-		if self.CHUDOptionLink then
-			self.CHUDOptionLink:SetIsVisible(false)
-		end
+
 	end)
 	
 originalMenuAnimations = Class_ReplaceMethod( "GUIMainMenu", "OnAnimationCompleted",
 	function(self, animatedItem, animationName, itemHandle)
-		if animationName == "ANIMATE_LINK_BG" then
-			if self.CHUDOptionLink then
-				self.CHUDOptionLink:SetFrameCount(15, 1.6, AnimateLinear, "ANIMATE_LINK_BG")
-			end
-			
-		elseif animationName == "MAIN_MENU_MOVE" then
-			if self.menuBackground:HasCSSClass("menu_bg_show") then
-				if self.CHUDOptionLink then
-					self.CHUDOptionLink:SetIsVisible(true)
-				end
-				
-				if self.CHUDNewsScript then
-					self.CHUDNewsScript:SetIsVisible(true)
-				end
-				
-			end
+		if animationName == "MAIN_MENU_MOVE" and self.menuBackground:HasCSSClass("menu_bg_show") and self.CHUDNewsScript then
+			self.CHUDNewsScript:SetIsVisible(true)
 		end
 
 		originalMenuAnimations(self, animatedItem, animationName, itemHandle)
@@ -159,17 +110,24 @@ originalMenuAnimations = Class_ReplaceMethod( "GUIMainMenu", "OnAnimationComplet
 originalMainMenuResChange = Class_ReplaceMethod( "GUIMainMenu", "OnResolutionChanged",
 	function(self, oldX, oldY, newX, newY)
 		originalMainMenuResChange(self, oldX, oldY, newX, newY)
-		for _, menuLink in pairs(menuLinks) do
-			menuLink:SetTopOffset(50+70*menuLink.linkNr)
+		for i, menuLink in pairs(self.Links) do
+			menuLink:SetTopOffset(50+70*(i-1))
 		end
 		
 		self.profileBackground:SetTopOffset(-70)
+		
+		if CHUDGetOption("mingui") then
+			mainMenu.mainWindow:SetBackgroundTexture("ui/transparent.dds")
+		else
+			mainMenu.mainWindow:SetBackgroundTexture("ui/menu/grid.dds")
+			mainMenu.mainWindow:SetBackgroundRepeat(true)
+		end
 	end)
 
 Client.PrecacheLocalSound("sound/chud.fev/CHUD/open_menu")
 	
 function MainMenu_OnOpenMenu()
-    StartSoundEffect("sound/chud.fev/CHUD/open_menu")
+	StartSoundEffect("sound/chud.fev/CHUD/open_menu")
 	mainMenu.tvGlareImage:SetIsVisible(false)
 	mainMenu.scanLine:SetIsVisible(not CHUDGetOption("mingui"))
 	
@@ -185,13 +143,13 @@ function MainMenu_OnOpenMenu()
 	else
 		// Solves issue where the news were visible when you click options and then spam escape
 		// This hides the news script properly
-		mainMenu.CHUDNewsScript:SetIsVisible(mainMenu.resumeLink:GetIsVisible())
+		mainMenu.CHUDNewsScript:SetIsVisible(mainMenu.Links[1]:GetIsVisible())
 	end
 
 end
 
 function MainMenu_OnCloseMenu()
-    Shared.StopSound(nil, "sound/chud.fev/CHUD/open_menu")
+	Shared.StopSound(nil, "sound/chud.fev/CHUD/open_menu")
 	
 	if mainMenu and mainMenu.CHUDNewsScript then
 		// Kill it, KILL IT WITH FIRE
@@ -203,12 +161,12 @@ end
 	
 function GUIMainMenu:CreateCHUDOptionWindow()
 
-    self.CHUDOptionWindow = self:CreateWindow()
-    self.CHUDOptionWindow:DisableCloseButton()
-    self.CHUDOptionWindow:SetCSSClass("option_window")
-    
-    self:SetupWindow(self.CHUDOptionWindow, "NS2+ OPTIONS")
-    local function InitOptionWindow()
+	self.CHUDOptionWindow = self:CreateWindow()
+	self.CHUDOptionWindow:DisableCloseButton()
+	self.CHUDOptionWindow:SetCSSClass("option_window")
+	
+	self:SetupWindow(self.CHUDOptionWindow, "NS2+ OPTIONS")
+	local function InitOptionWindow()
 		for idx, option in pairs(CHUDOptions) do
 			if option.valueType == "bool" then
 				self.CHUDOptionElements[option.name]:SetOptionActive( BoolToIndex(CHUDOptions[idx].currentValue) )
@@ -230,39 +188,39 @@ function GUIMainMenu:CreateCHUDOptionWindow()
 			background:SetLayer(bgLayer+2)
 		end
 		
-    end
-    self.CHUDOptionWindow:AddEventCallbacks({ OnHide = InitOptionWindow })
-    
-    local content = self.CHUDOptionWindow:GetContentBox()
-    
-    local back = CreateMenuElement(self.CHUDOptionWindow, "MenuButton")
-    back:SetCSSClass("back")
-    back:SetText("BACK")
-    back:AddEventCallbacks( { OnClick = function() self.CHUDOptionWindow:SetIsVisible(false) end } )
-    
-    local apply = CreateMenuElement(self.CHUDOptionWindow, "MenuButton")
-    apply:SetCSSClass("apply")
-    apply:SetText("APPLY")
-    apply:AddEventCallbacks( { OnClick = function() CHUDSaveMenuSettings() end } )
+	end
+	self.CHUDOptionWindow:AddEventCallbacks({ OnHide = InitOptionWindow })
+	
+	local content = self.CHUDOptionWindow:GetContentBox()
+	
+	local back = CreateMenuElement(self.CHUDOptionWindow, "MenuButton")
+	back:SetCSSClass("back")
+	back:SetText("BACK")
+	back:AddEventCallbacks( { OnClick = function() self.CHUDOptionWindow:SetIsVisible(false) end } )
+	
+	local apply = CreateMenuElement(self.CHUDOptionWindow, "MenuButton")
+	apply:SetCSSClass("apply")
+	apply:SetText("APPLY")
+	apply:AddEventCallbacks( { OnClick = function() CHUDSaveMenuSettings() end } )
 
-    self.fpsDisplay = CreateMenuElement( self.CHUDOptionWindow, "MenuButton" )
-    self.fpsDisplay:SetCSSClass("fps")
+	self.fpsDisplay = CreateMenuElement( self.CHUDOptionWindow, "MenuButton" )
+	self.fpsDisplay:SetCSSClass("fps")
 	
 	local changelogButton = CreateMenuElement( self.CHUDOptionWindow, "MenuButton" )
 	changelogButton:SetCSSClass("apply")
 	changelogButton:SetLeftOffset(0)
 	changelogButton:SetText("CHANGELOG")
 	local kChangeURL = "http://steamcommunity.com/sharedfiles/filedetails/changelog/135458820"
-    changelogButton:AddEventCallbacks( { OnClick = function() Client.ShowWebpage(kChangeURL) end } )
+	changelogButton:AddEventCallbacks( { OnClick = function() Client.ShowWebpage(kChangeURL) end } )
 	
-    self.warningLabel = CreateMenuElement(self.CHUDOptionWindow, "MenuButton", false)
-    self.warningLabel:SetCSSClass("warning_label")
-    self.warningLabel:SetText("Game restart required")
-    self.warningLabel:SetIgnoreEvents(true)
-    self.warningLabel:SetIsVisible(false)
+	self.warningLabel = CreateMenuElement(self.CHUDOptionWindow, "MenuButton", false)
+	self.warningLabel:SetCSSClass("warning_label")
+	self.warningLabel:SetText("Game restart required")
+	self.warningLabel:SetIgnoreEvents(true)
+	self.warningLabel:SetIsVisible(false)
 
-    // save our option elements for future reference
-    self.CHUDOptionElements = { }
+	// save our option elements for future reference
+	self.CHUDOptionElements = { }
 	
 	local HUDOptionsMenu = { }
 	local FuncOptionsMenu = { }
@@ -292,82 +250,82 @@ function GUIMainMenu:CreateCHUDOptionWindow()
 		table.sort(FuncOptionsMenu, CHUDOptionsSort)
 		table.sort(CompOptionsMenu, CHUDOptionsSort)
 	end
-    
-    local CHUD_HUDForm = GUIMainMenu.CreateCHUDOptionsForm(self, content, HUDOptionsMenu, self.CHUDOptionElements)
+	
+	local CHUD_HUDForm = GUIMainMenu.CreateCHUDOptionsForm(self, content, HUDOptionsMenu, self.CHUDOptionElements)
 	local CHUD_FuncForm = GUIMainMenu.CreateCHUDOptionsForm(self, content, FuncOptionsMenu, self.CHUDOptionElements)
 	local CHUD_CompForm = GUIMainMenu.CreateCHUDOptionsForm(self, content, CompOptionsMenu, self.CHUDOptionElements)
 
-    local tabs = 
-        {
+	local tabs = 
+		{
 			{ label = "VISUAL", form = CHUD_FuncForm, scroll=true  },
 			{ label = "HUD", form = CHUD_HUDForm, scroll=true  },
 			{ label = "MISC", form = CHUD_CompForm, scroll=true  },
-        }
-        
-    local xTabWidth = 256
+		}
+		
+	local xTabWidth = 256
 
-    local tabBackground = CreateMenuElement(self.CHUDOptionWindow, "Image")
-    tabBackground:SetCSSClass("tab_background")
-    tabBackground:SetIgnoreEvents(true)
-    
-    local tabAnimateTime = 0.1
-        
-    for i = 1,#tabs do
-    
-        local tab = tabs[i]
-        local tabButton = CreateMenuElement(self.CHUDOptionWindow, "MenuButton")
-        
-        local function ShowTab()
-            for j =1,#tabs do
-                tabs[j].form:SetIsVisible(i == j)
-                self.CHUDOptionWindow:ResetSlideBar()
-                self.CHUDOptionWindow:SetSlideBarVisible(tab.scroll == true)
-                local tabPosition = tabButton.background:GetPosition()
-                tabBackground:SetBackgroundPosition( tabPosition, false, tabAnimateTime ) 
-            end
-        end
-    
-        tabButton:SetCSSClass("tab")
-        tabButton:SetText(tab.label)
-        tabButton:AddEventCallbacks({ OnClick = ShowTab })
-        
-        local tabWidth = tabButton:GetWidth()
-        tabButton:SetBackgroundPosition( Vector(tabWidth * (i - 1), 0, 0) )
-        
-        // Make the first tab visible.
-        if i==1 then
-            tabBackground:SetBackgroundPosition( Vector(tabWidth * (i - 1), 0, 0) )
-            ShowTab()
-        end
-        
-    end        
-    
-    InitOptionWindow()
+	local tabBackground = CreateMenuElement(self.CHUDOptionWindow, "Image")
+	tabBackground:SetCSSClass("tab_background")
+	tabBackground:SetIgnoreEvents(true)
+	
+	local tabAnimateTime = 0.1
+		
+	for i = 1,#tabs do
+	
+		local tab = tabs[i]
+		local tabButton = CreateMenuElement(self.CHUDOptionWindow, "MenuButton")
+		
+		local function ShowTab()
+			for j =1,#tabs do
+				tabs[j].form:SetIsVisible(i == j)
+				self.CHUDOptionWindow:ResetSlideBar()
+				self.CHUDOptionWindow:SetSlideBarVisible(tab.scroll == true)
+				local tabPosition = tabButton.background:GetPosition()
+				tabBackground:SetBackgroundPosition( tabPosition, false, tabAnimateTime ) 
+			end
+		end
+	
+		tabButton:SetCSSClass("tab")
+		tabButton:SetText(tab.label)
+		tabButton:AddEventCallbacks({ OnClick = ShowTab })
+		
+		local tabWidth = tabButton:GetWidth()
+		tabButton:SetBackgroundPosition( Vector(tabWidth * (i - 1), 0, 0) )
+		
+		// Make the first tab visible.
+		if i==1 then
+			tabBackground:SetBackgroundPosition( Vector(tabWidth * (i - 1), 0, 0) )
+			ShowTab()
+		end
+		
+	end        
+	
+	InitOptionWindow()
   
 end
 
 GUIMainMenu.CreateCHUDOptionsForm = function(mainMenu, content, options, optionElements)
 
-    local form = CreateMenuElement(content, "Form", false)
-    
-    local rowHeight = 50
+	local form = CreateMenuElement(content, "Form", false)
+	
+	local rowHeight = 50
 	
 	local y = 0
-    
-    for _, option in ipairs(options) do
-    
-        local input
+	
+	for _, option in ipairs(options) do
+	
+		local input
 		local input_display
-        local defaultInputClass = "option_input"
+		local defaultInputClass = "option_input"
 		local multiplier = option.multiplier or 1
 		local minValue = option.minValue or 0
 		local maxValue = option.maxValue or 1
 		
-        if option.type == "select" then
-            input = form:CreateFormElement(Form.kElementType.DropDown, option.name, option.value)
-            if option.values then
-                input:SetOptions(option.values)
-            end                
+		if option.type == "select" then
+			input = form:CreateFormElement(Form.kElementType.DropDown, option.name, option.value)
+			if option.values then
+				input:SetOptions(option.values)
+			end                
 			if option.name == "CHUD_Hitsounds" then
 				local soundPreview = CreateMenuElement(form, "MenuButton", false)
 				soundPreview:SetCSSClass("clear_keybind")
@@ -399,8 +357,8 @@ GUIMainMenu.CreateCHUDOptionsForm = function(mainMenu, content, options, optionE
 				end
 			end
 			
-        elseif option.type == "slider" then
-            input = form:CreateFormElement(Form.kElementType.SlideBar, option.name, option.value)
+		elseif option.type == "slider" then
+			input = form:CreateFormElement(Form.kElementType.SlideBar, option.name, option.value)
 			input_display = form:CreateFormElement(Form.kElementType.TextInput, option.name, option.value)
 			input_display:SetNumbersOnly(true)	
 			input_display:SetXAlignment(GUIItem.Align_Min)
@@ -435,42 +393,42 @@ GUIMainMenu.CreateCHUDOptionsForm = function(mainMenu, content, options, optionE
 				end
 			end,
 			})
-            // HACK: Really should use input:AddSetValueCallback, but the slider bar bypasses that.
-            if option.sliderCallback then
-                input:Register(
-                    {OnSlide =
-                        function(value, interest)
-                            option.sliderCallback(mainMenu)
+			// HACK: Really should use input:AddSetValueCallback, but the slider bar bypasses that.
+			if option.sliderCallback then
+				input:Register(
+					{OnSlide =
+						function(value, interest)
+							option.sliderCallback(mainMenu)
 							local value = (input:GetValue() * (maxValue - minValue) + minValue) * multiplier
 							input_display:SetValue(ToString(string.sub(value, 0, ConditionalValue(value > 100, 5, 4))))
-                        end
-                    }, SLIDE_HORIZONTAL)
-            end
-        elseif option.type == "progress" then
-            input = form:CreateFormElement(Form.kElementType.ProgressBar, option.name, option.value)       
-        elseif option.type == "checkbox" then
-            input = form:CreateFormElement(Form.kElementType.Checkbox, option.name, option.value)
-            defaultInputClass = "option_checkbox"
-        else
-            input = form:CreateFormElement(Form.kElementType.TextInput, option.name, option.value)
-        end
-        
-        if option.callback then
-            input:AddSetValueCallback(option.callback)
-        end
-        local inputClass = defaultInputClass
-        if option.inputClass then
-            inputClass = option.inputClass
-        end
-        
-        input:SetCSSClass(inputClass)
-        input:SetTopOffset(y)
+						end
+					}, SLIDE_HORIZONTAL)
+			end
+		elseif option.type == "progress" then
+			input = form:CreateFormElement(Form.kElementType.ProgressBar, option.name, option.value)       
+		elseif option.type == "checkbox" then
+			input = form:CreateFormElement(Form.kElementType.Checkbox, option.name, option.value)
+			defaultInputClass = "option_checkbox"
+		else
+			input = form:CreateFormElement(Form.kElementType.TextInput, option.name, option.value)
+		end
 		
-        local label = CreateMenuElement(form, "Font", false)
-        label:SetCSSClass("option_label")
-        label:SetText(string.upper(option.label) .. ":")
-        label:SetTopOffset(y)
-        label:SetIgnoreEvents(false)
+		if option.callback then
+			input:AddSetValueCallback(option.callback)
+		end
+		local inputClass = defaultInputClass
+		if option.inputClass then
+			inputClass = option.inputClass
+		end
+		
+		input:SetCSSClass(inputClass)
+		input:SetTopOffset(y)
+		
+		local label = CreateMenuElement(form, "Font", false)
+		label:SetCSSClass("option_label")
+		label:SetText(string.upper(option.label) .. ":")
+		label:SetTopOffset(y)
+		label:SetIgnoreEvents(false)
 		
 		local tooltipCallbacks = { 
 			OnMouseOver = function(self)
@@ -482,11 +440,11 @@ GUIMainMenu.CreateCHUDOptionsForm = function(mainMenu, content, options, optionE
 							text = text .. " (Disabled by server)."
 						end
 						
-						local cutoff = 60
+						local cutoff = 300
 						
-						mainMenu.optionTooltip.tooltip:SetText(WrapText( text, cutoff, nil, "" ))
+						mainMenu.optionTooltip.tooltip:SetText(WordWrap(mainMenu.optionTooltip.tooltip, text, 0, cutoff))
 						
-						mainMenu.optionTooltip.tooltip:SetPosition(Vector(15, ConditionalValue(string.len(text) <= cutoff, 0, -10), 0))
+						mainMenu.optionTooltip.tooltip:SetPosition(Vector(15, ConditionalValue(string.find(text, "\n"), 0, -10), 0))
 					else
 						mainMenu.optionTooltip.tooltip:SetText("")
 					end
@@ -504,17 +462,17 @@ GUIMainMenu.CreateCHUDOptionsForm = function(mainMenu, content, options, optionE
 		for _, child in ipairs(input.children) do
 			child:AddEventCallbacks(tooltipCallbacks)
 		end
-        
-        optionElements[option.name] = input
+		
+		optionElements[option.name] = input
 		optionElements[option.name].label = label
 		
 		y = y + rowHeight
 
-    end
-    
-    form:SetCSSClass("options")
+	end
+	
+	form:SetCSSClass("options")
 	form:SetHeight(y)
 
-    return form
+	return form
 
 end
