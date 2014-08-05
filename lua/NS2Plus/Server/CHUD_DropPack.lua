d8d8cf2a668e0a02bd5b33c18f7f67fd1ad8c9ae
Mendasp <mendasp@gmail.com>
2014-08-06 00:22:41 +0200

    Readded missing code to count dropped meds/ammo/catpacks :D

--- lua/NS2Plus/Server/CHUD_DropPack.lua
+++ lua/NS2Plus/Server/CHUD_DropPack.lua
@@ -1,3 +1,18 @@
+local commDrops = { "ammopack", "medpack", "catpack" }
+local originalDropPackOnInit
+originalDropPackOnInit = Class_ReplaceMethod( "DropPack", "OnInitialized",
+	function(self)
+	
+		originalDropPackOnInit(self)
+		
+		local mapName = self:GetMapName()
+		
+		if table.contains(commDrops, mapName) then
+			CHUDCommStats[CHUDMarineComm][mapName].misses = CHUDCommStats[CHUDMarineComm][mapName].misses + 1
+		end
+	
+	end)
+
 originalDropPackOnUpdate = Class_ReplaceMethod( "DropPack", "OnUpdate",
 	function(self, deltaTime)
 		-- GetEntitiesForTeamWithinXZRange ignores the Y axis
