e95d5abcb59b9d16ef435a759a11ec6733532024
Mendasp <mendasp@gmail.com>
2014-08-22 20:04:51 +0200

    Updated dev build number.

--- lua/NS2Plus/CHUD_Shared.lua
+++ lua/NS2Plus/CHUD_Shared.lua
@@ -1,7 +1,7 @@
 kCHUDElixerVersion = 1.72
 // Version number is the amount of revisions in the Workshop
 // Try to update but only important when changing defaults
-kCHUDVersion = 200
+kCHUDVersion = 202
 
 Script.Load("lua/NS2Plus/Shared/CHUD_Utility.lua")
 Script.Load("lua/NS2Plus/Elixer_Utility.lua")
