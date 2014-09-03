d5f13283c255910ef0d2462931c69d320d4a4fb5
Mendasp <mendasp@gmail.com>
2014-09-04 00:37:54 +0200

    Removed fade blink sound reduction

--- lua/NS2Plus/Client/CHUD_Sounds.lua
+++ lua/NS2Plus/Client/CHUD_Sounds.lua
@@ -18,15 +18,9 @@ local blockedVO = set {
 					"sound/NS2.fev/marine/voiceovers/commander/defend",
 					"sound/NS2.fev/marine/voiceovers/move" }
 					
-local reducedSounds = set {
-					"sound/NS2.fev/alien/fade/blink",
-					"sound/NS2.fev/alien/fade/blink_end" }
 Script.Load("lua/SoundEffect.lua")
 function StartSoundEffectOnEntity(soundEffectName, onEntity, volume, predictor)
 	if not blockedVO[soundEffectName] or CHUDGetOption("wps") then
-		if reducedSounds[soundEffectName] then
-			volume = volume and volume * 0.8 or 0.8
-		end
 		Shared.PlaySound(onEntity, soundEffectName, volume or 1)
 	end
 end
@@ -36,21 +30,3 @@ function StartSoundEffect(soundEffectName, volume)
 		Shared.PlaySound(nil, soundEffectName, volume or 1)
 	end
 end
-
-local kBlink2DSound = PrecacheAsset("sound/NS2.fev/alien/fade/blink_loop")
-Class_ReplaceMethod("Fade", "UpdateBlink2DSound",
-	function(self)
-		local playSound = self:GetIsBlinking() and not GetHasSilenceUpgrade(self)
-
-		if playSound and not self.blinkSoundPlaying then
-		
-			Shared.PlaySound(self, kBlink2DSound, 0.6)
-			self.blinkSoundPlaying = true
-			
-		elseif not playSound and self.blinkSoundPlaying then
-		
-			Shared.StopSound(self, kBlink2DSound)
-			self.blinkSoundPlaying = false
-			
-		end
-	end)
