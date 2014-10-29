f3936cc16056cc6132b1a17d054b0295b0429ae5
Mendasp <mendasp@gmail.com>
2014-10-30 00:05:50 +0100

    Team skills console message was backwards

--- lua/NS2Plus/Server/CHUD_GameInfo.lua
+++ lua/NS2Plus/Server/CHUD_GameInfo.lua
@@ -5,7 +5,7 @@ originalGameInfo = Class_ReplaceMethod( "GameInfo", "OnCreate",
 		originalGameInfo(self)
 		
 		self.showAvgSkill = CHUDServerOptions["showavgteamskill"].currentValue == true
-		local showAvgSkill = ConditionalValue(self.showAvgSkill, "Disabled", "Enabled")
+		local showAvgSkill = ConditionalValue(self.showAvgSkill, "Enabled", "Disabled")
 		Shared.Message("[NS2+] Display team avg. skill: " .. showAvgSkill)
 		
 	end)
