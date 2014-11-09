7db7e70a55b86728cfbc35ce75d36fd844173061
Mendasp <mendasp@gmail.com>
2014-11-09 13:22:45 +0100

    Bump version to 232.

--- lua/NS2Plus/CHUD_Shared.lua
+++ lua/NS2Plus/CHUD_Shared.lua
@@ -1,7 +1,7 @@
 kCHUDElixerVersion = 1.8
 // Version number is the amount of revisions in the Workshop
 // Try to update but only important when changing defaults
-kCHUDVersion = 231
+kCHUDVersion = 232
 
 Script.Load("lua/NS2Plus/Shared/CHUD_Utility.lua")
 Script.Load("lua/NS2Plus/Elixer_Utility.lua")
