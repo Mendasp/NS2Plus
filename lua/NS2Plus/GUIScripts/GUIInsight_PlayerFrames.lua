cc2e77c947544cb8edc63952a64d88427d04b53a
Mendasp <mendasp@gmail.com>
2015-03-27 22:01:36 +0100

    Hook the function properly.

--- lua/NS2Plus/GUIScripts/GUIInsight_PlayerFrames.lua
+++ lua/NS2Plus/GUIScripts/GUIInsight_PlayerFrames.lua
@@ -1,18 +1,19 @@
 Script.Load("lua/GUIInsight_PlayerFrames.lua")
 
-local originalIPFUpdatePlayer = GUIInsight_PlayerFrames.UpdatePlayer
-function GUIInsight_PlayerFrames:UpdatePlayer(player, playerRecord, team, yPosition)
-	originalIPFUpdatePlayer(self, player, playerRecord, team, yPosition)
-	
-	for _, playerInfo in ientitylist(Shared.GetEntitiesWithClassname("PlayerInfoEntity")) do
-		if playerInfo.playerId == playerRecord.EntityId then
-			if GUIItemContainsPoint(player["Background"], Client.GetCursorPosScreen()) and playerRecord.Status ~= Locale.ResolveString("STATUS_DEAD") then
-				local text = playerRecord.Health .. " / " .. playerRecord.Armor
-				if playerRecord.Status == Locale.ResolveString("STATUS_EXO") then
-					text = tostring(playerRecord.Armor)
+local originalIPFUpdatePlayer
+originalIPFUpdatePlayer = Class_ReplaceMethod("GUIInsight_PlayerFrames", "UpdatePlayer",
+	function(self, player, playerRecord, team, yPosition)
+		originalIPFUpdatePlayer(self, player, playerRecord, team, yPosition)
+		
+		for _, playerInfo in ientitylist(Shared.GetEntitiesWithClassname("PlayerInfoEntity")) do
+			if playerInfo.playerId == playerRecord.EntityId then
+				if GUIItemContainsPoint(player["Background"], Client.GetCursorPosScreen()) and playerRecord.Status ~= Locale.ResolveString("STATUS_DEAD") then
+					local text = playerRecord.Health .. " / " .. playerRecord.Armor
+					if playerRecord.Status == Locale.ResolveString("STATUS_EXO") then
+						text = tostring(playerRecord.Armor)
+					end
+					player["Detail"]:SetText(text)
 				end
-				player["Detail"]:SetText(text)
 			end
 		end
-	end
-end
+	end)
