131c2cb20569699fc3c3a8e0e1fa9f4ce5f3dbad
mendasp <mendasp@gmail.com>
2015-04-27 17:19:46 +0200

    Make sure the values exist or display 0

--- lua/NS2Plus/GUIScripts/GUIInsight_PlayerFrames.lua
+++ lua/NS2Plus/GUIScripts/GUIInsight_PlayerFrames.lua
@@ -8,9 +8,9 @@ originalIPFUpdatePlayer = Class_ReplaceMethod("GUIInsight_PlayerFrames", "Update
 		for _, playerInfo in ientitylist(Shared.GetEntitiesWithClassname("PlayerInfoEntity")) do
 			if playerInfo.playerId == playerRecord.EntityId then
 				if GUIItemContainsPoint(player["Background"], Client.GetCursorPosScreen()) and playerRecord.Status ~= Locale.ResolveString("STATUS_DEAD") then
-					local text = playerRecord.Health .. " / " .. playerRecord.Armor
+					local text = playerRecord.Health or 0 .. " / " .. playerRecord.Armor or 0
 					if playerRecord.Status == Locale.ResolveString("STATUS_EXO") then
-						text = tostring(playerRecord.Armor)
+						text = tostring(playerRecord.Armor or 0)
 					end
 					player["Detail"]:SetText(text)
 				end
