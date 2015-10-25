18e50925046355adc54da92ca110a245977353b7
mendasp <mendasp@gmail.com>
2015-10-25 23:52:46 +0100

    Removed calls to debug functions.

--- lua/NS2Plus/GUIScripts/GUIScoreboard.lua
+++ lua/NS2Plus/GUIScripts/GUIScoreboard.lua
@@ -1,5 +1,5 @@
 local kNS2StatsProfileURL = "http://ns2stats.com/player/ns2id/"
-local team1Skill, team2Skill
+local team1Skill, team2Skill, textHeight, teamItemWidth
 
 local originalScoreboardUpdateTeam
 originalScoreboardUpdateTeam = Class_ReplaceMethod( "GUIScoreboard", "UpdateTeam",
@@ -87,6 +87,12 @@ function(self)
 	self.avgSkillItem2:SetText("")
 	self.avgSkillItem2:SetLayer(kGUILayerScoreboard)
 	GUIMakeFontScale(self.avgSkillItem2)
+	
+	teamItemWidth = self.teams[1].GUIs.Background:GetSize().x
+	textHeight = self.avgSkillItem:GetTextHeight("Avg") * self.avgSkillItem:GetScale().y
+	
+	self.avgSkillItemBg:SetSize(Vector(teamItemWidth, textHeight+5*GUIScoreboard.kScalingFactor, 0))
+	self.avgSkillItem2Bg:SetSize(Vector(teamItemWidth, textHeight+5*GUIScoreboard.kScalingFactor, 0))
 end)
 
 local originalScoreboardUpdate
@@ -105,15 +111,9 @@ function(self, deltaTime)
 		self.showAvgSkill = GetGameInfoEntity().showAvgSkill
 
 		if self.showAvgSkill == true then
-			local GetTeamItemWidth = GetUpValue( GUIScoreboard.Update, "GetTeamItemWidth", { LocateRecurse = true } )
-			
 			local team1Players = #self.teams[2]["GetScores"]()
 			local team2Players = #self.teams[3]["GetScores"]()
 			local hasText = false
-
-			-- Check if the teams are on top of each other or not
-			local isVerticalSB = GetTeamItemWidth()*2 > self.scoreboardBackground:GetSize().x
-			local textHeight = self.avgSkillItem:GetTextHeight("Avg") * self.avgSkillItem:GetScale().y
 			
 			self.avgSkillItemBg:SetIsVisible(true)
 			self.avgSkillItem2Bg:SetIsVisible(true)
@@ -128,10 +128,10 @@ function(self, deltaTime)
 				self.avgSkillItem2:SetText(team2Text)
 				hasText = true
 				
-				local team1TextWidth = self.avgSkillItem:GetTextWidth(self.avgSkillItem:GetText()) * self.avgSkillItem:GetScale().x
-				local team2TextWidth = self.avgSkillItem2:GetTextWidth(self.avgSkillItem2:GetText()) * self.avgSkillItem2:GetScale().x
-				
-				if isVerticalSB then
+				if teamItemWidth*2 > self.scoreboardBackground:GetSize().x then
+					local team1TextWidth = self.avgSkillItem:GetTextWidth(self.avgSkillItem:GetText()) * self.avgSkillItem:GetScale().x
+					local team2TextWidth = self.avgSkillItem2:GetTextWidth(self.avgSkillItem2:GetText()) * self.avgSkillItem2:GetScale().x
+					
 					self.avgSkillItem:SetPosition(Vector(-20*GUIScoreboard.kScalingFactor-team1TextWidth/2, textHeight/2+5*GUIScoreboard.kScalingFactor, 0))
 					self.avgSkillItem2:SetPosition(Vector(20*GUIScoreboard.kScalingFactor+team2TextWidth/2, textHeight/2+5*GUIScoreboard.kScalingFactor, 0))
 					self.avgSkillItem2Bg:SetIsVisible(false)
@@ -174,12 +174,6 @@ function(self, deltaTime)
 					sliderbarBgYSize = sliderbarBgYSize-textHeight
 				end
 				
-				local team1Width = self.teams[2].GUIs.Background:GetSize().x
-				local team2Width = self.teams[3].GUIs.Background:GetSize().x
-				
-				self.avgSkillItemBg:SetSize(Vector(team1Width, textHeight+5*GUIScoreboard.kScalingFactor, 0))
-				self.avgSkillItem2Bg:SetSize(Vector(team2Width, textHeight+5*GUIScoreboard.kScalingFactor, 0))
-				
 				self.avgSkillItemBg:SetPosition(Vector(self.teams[2].GUIs.Background:GetPosition().x, ConditionalValue(GUIScoreboard.kScalingFactor == 1, 5*GUIScoreboard.kScalingFactor, 0), 0))
 				self.avgSkillItem2Bg:SetPosition(Vector(self.teams[3].GUIs.Background:GetPosition().x,  ConditionalValue(GUIScoreboard.kScalingFactor == 1, 5*GUIScoreboard.kScalingFactor, 0), 0))
 				
@@ -198,12 +192,16 @@ function(self, deltaTime)
 end)
 
 local originalLocaleResolveString = Locale.ResolveString
-function Locale.ResolveString(string)
-	if string == "SB_ASSISTS" and CHUDGetOption("kda") then
-		return originalLocaleResolveString("SB_DEATHS")
-	elseif string == "SB_DEATHS" and CHUDGetOption("kda") then
-		return originalLocaleResolveString("SB_ASSISTS")
+function Locale.ResolveString(resolveString)
+	if CHUDGetOption("kda") then
+		if resolveString == "SB_ASSISTS" then
+			return originalLocaleResolveString("SB_DEATHS")
+		elseif resolveString == "SB_DEATHS" then
+			return originalLocaleResolveString("SB_ASSISTS")
+		else
+			return originalLocaleResolveString(resolveString)
+		end
 	else
-		return originalLocaleResolveString(string)
+		return originalLocaleResolveString(resolveString)
 	end
 end
