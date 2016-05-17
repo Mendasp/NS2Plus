f2476a10ead49856883479f416e95b612a97a4c9
sclark39 <sclark@freolic.com>
2016-05-17 16:32:51 -0400

    Adding notifications to update to the new modid

--- lua/NS2Plus/CHUD_Server.lua
+++ lua/NS2Plus/CHUD_Server.lua
@@ -54,3 +54,7 @@ else
 end
 
 Shared.Message("----------------------------------")
+	for i=1,25 do
+		Shared.Message( "NS2+ has moved! Please update servers to modID 28eb0f83")
+	end
+Shared.Message("----------------------------------")
--- lua/NS2Plus/Client/CHUD_MainMenuHooks.lua
+++ lua/NS2Plus/Client/CHUD_MainMenuHooks.lua
@@ -21,6 +21,14 @@ function MainMenu_Open()
 			Shared.Message("NS2+ has been updated or is not available, not loading main menu mods. A restart will be required when the update is installed (you can do it from the mods menu).")
 		end
 	end
-	
+		
+	Shared.Message("----------------------------------")
+	for i=1,5 do
+		Shared.Message( "NS2+ has moved! Please run 'plus_workshop' to open the overlay to the new mod, and ask server ops to update to 28eb0f83.")
+	end
+	Shared.Message("----------------------------------")
+
 	oldMainMenu_Open()
 end
+
+Event.Hook( "Console_plus_workshop", function() Client.ShowWebpage( "http://steamcommunity.com/sharedfiles/filedetails/?id=686493571" ) end )
