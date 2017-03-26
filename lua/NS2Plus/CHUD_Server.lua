706eaad62c90cf679f220cef3c43991f98bc6c18
sclark39 <sclark@freolic.com>
2017-03-26 15:00:17 -0400

    Update to point to new mod id

--- lua/NS2Plus/CHUD_Server.lua
+++ lua/NS2Plus/CHUD_Server.lua
@@ -1,5 +1,5 @@
 Shared.Message("----------------------------------")
 	for i=1,25 do
-		Shared.Message( "NS2+ has moved! Please update servers to modID 28eb0f83")
+		Shared.Message( "NS2+ has moved! Please update servers to modID 334982d2")
 	end
 Shared.Message("----------------------------------")
--- lua/NS2Plus/Client/CHUD_MainMenuHooks.lua
+++ lua/NS2Plus/Client/CHUD_MainMenuHooks.lua
@@ -2,10 +2,10 @@ local oldMainMenu_Open = MainMenu_Open
 function MainMenu_Open()
 	Shared.Message("----------------------------------")
 	for i=1,5 do
-		Shared.Message( "NS2+ has moved! Please run 'plus_workshop' to open the overlay to the new mod, and ask server ops to update to 28eb0f83.")
+		Shared.Message( "NS2+ has moved! Please run 'plus_workshop' to open the overlay to the new mod, and ask server ops to update to 334982d2.")
 	end
 	Shared.Message("----------------------------------")
 	oldMainMenu_Open()
 end
 
-Event.Hook( "Console_plus_workshop", function() Client.ShowWebpage( "http://steamcommunity.com/sharedfiles/filedetails/?id=686493571" ) end )
+Event.Hook( "Console_plus_workshop", function() Client.ShowWebpage( "http://steamcommunity.com/sharedfiles/filedetails/?id=860455634" ) end )
