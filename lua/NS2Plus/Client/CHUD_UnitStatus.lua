d30532e87d42f43da3315e0cdd8e8d321dd5903e
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2018-03-26 17:30:00 +0200

    Fixed that the unitstatus's CHUDBlipData weren't updated with every frame (closes #73)

--- lua/NS2Plus/Client/CHUD_UnitStatus.lua
+++ lua/NS2Plus/Client/CHUD_UnitStatus.lua
@@ -1,33 +1,48 @@
+function UnitStatusMixin:GetCHUDBlipData(forEntity, hint)
+	local nameplates = CHUDGetOption("nameplates") or 0
+	if nameplates == 0 then return hint end
+
+	local player = Client.GetLocalPlayer()
+	if not HasMixin(self, "Live") or self.GetShowHealthFor and not self:GetShowHealthFor(player) then
+		return hint
+	end
+
+	local CHUDBlipData = { }
+
+	CHUDBlipData.Hint = hint
+
+	local status = string.format("%d/%d",math.max(1, math.ceil(self:GetHealth())),math.ceil(self:GetArmor()))
+	if self:isa("Exo") or self:isa("Exosuit") then
+		status = string.format("%d",math.max(1, math.ceil(self:GetArmor())))
+	end
+	CHUDBlipData.Status = status
+
+	if (self:GetMapName() ~= TechPoint.kMapName and self:GetMapName() ~= ResourcePoint.kPointMapName) then
+		if not self:isa("Player") or (self:isa("Embryo") and GetAreEnemies(player, self)) then
+			CHUDBlipData.Percentage = string.format("%d%%",math.max(1, math.ceil(self:GetHealthScalar()*100)))
+		end
+	end
+
+	if self:isa("Marine") then
+		CHUDBlipData.HasWelder = self:GetHasWelder(player)
+	end
+
+	return CHUDBlipData
+end
+
 local originalGetUnitHint = UnitStatusMixin.GetUnitHint
 function UnitStatusMixin:GetUnitHint(forEntity)
-	
-	local player = Client.GetLocalPlayer()
 	local hint = originalGetUnitHint(self, forEntity)
 	
-	if HasMixin(self, "Live") and (not self.GetShowHealthFor or self:GetShowHealthFor(player)) then
+	return self:GetCHUDBlipData(forEntity, hint)
+end
 
-		local hintTable = { }
-		
-		hintTable.Hint = hint
-		
-		local status = string.format("%d/%d",math.max(1, math.ceil(self:GetHealth())),math.ceil(self:GetArmor()))
-		if self:isa("Exo") or self:isa("Exosuit") then
-			status = string.format("%d",math.max(1, math.ceil(self:GetArmor())))
-		end
-		hintTable.Status = status
-		
-		if (self:GetMapName() ~= TechPoint.kMapName and self:GetMapName() ~= ResourcePoint.kPointMapName) then
-			if not self:isa("Player") or (self:isa("Embryo") and GetAreEnemies(player, self)) then
-				hintTable.Percentage = string.format("%d%%",math.max(1, math.ceil(self:GetHealthScalar()*100)))
-			end
-		end
-		
-		if self:isa("Marine") then
-			hintTable.HasWelder = self:GetHasWelder(player)
-		end
-		
-		return hintTable
+local oldGetUnitState = UnitStatusMixin.GetUnitState
+function UnitStatusMixin:GetUnitState(forEntity)
+	local state = oldGetUnitState(self, forEntity)
+	if state and state.CHUDBlipData then
+		state.CHUDBlipData = self:GetCHUDBlipData(forEntity)
 	end
-	
-	return hint
+
+	return state
 end
